f62b6fd0de0f124b0ae4fd9c0e653363b3e7d2bf
menegattb <menegattb@gmail.com>
2024-12-12 23:11:15 -0300

    Arquivos .py Separados, funcionando!

---
+++ dharma_transcriptions/utils.py
@@ -0,0 +1,11 @@
+import re
+
+def sanitize_filename(filename):
+    return re.sub(r'[<>:"/\\|?*]', '_', filename)
+
+def format_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = int(seconds % 60)
+    milliseconds = int((seconds % 1) * 1000)
+    return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
